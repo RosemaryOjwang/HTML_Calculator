70d5ffbdd2c01883035143eebe1377acb63cf563
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2022-11-14 12:20:00 +0300

    declare function calculate() and function getUserInput()

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,17 @@ function multiplication() {
   let number2;
   return number1 * number2;
 }
+
+//calculator function
+function calculate() {
+}
+function getUserInput() {
+  let userInput = document.getElementsByName("userInput");
+  let inputValue;
+  for (i = 0; i < userInput.length; i++) {
+    if (userInput[i].checked) {
+      inputValue = userInput[i].value;
+    }
+  }
+  return inputValue;
+}
