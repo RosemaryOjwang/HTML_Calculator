0abcfe7123b1a8a31b9b9c3f37b2b1ca5a3a6fee
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2022-11-14 12:22:31 +0300

    declare function calculate() and function getUserInput()

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,4 @@
 //Business logic
-//A function to perfom multiplication
-function multiplication() {
-  let number1;
-  let number2;
-  return number1 * number2;
-}
-
 //calculator function
 function calculate() {
 }
