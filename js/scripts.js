65170d2f30a18a04ef1a813ca44c9391043c5e44
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2022-12-13 09:43:33 +0300

    replace event handlers with eventListeners and add regular expression to check expression validity

--- js/scripts.js
+++ js/scripts.js
@@ -1,99 +1,101 @@
 
 window.onload = function () {
-    //const table = document.querySelector("table");
-    //table.onclick = funtion(Event)  {
-    // Event.preventDefault();
     const seven = document.getElementById("seven");
-    seven.onclick = function () {
+    seven.addEventListener("click", function () {
         document.getElementById("resultsDisplay").innerText += "7";
-    };
+    }
+    );
 
     const eight = document.getElementById("eight");
-    eight.onclick = function () {
+    eight.addEventListener("click", function () {
         document.getElementById("resultsDisplay").innerText += "8";
-    };
+    }
+    );
     const nine = document.getElementById("nine");
-    nine.onclick = function () {
+    nine.addEventListener("click", function () {
         document.getElementById("resultsDisplay").innerText += "9";
-    };
+    }
+    );
     const zero = document.getElementById("zero");
-    zero.onclick = function () {
+    zero.addEventListener("click", function () {
         document.getElementById("resultsDisplay").innerText += "0";
-    };
+    }
+    );
     const one = document.getElementById("one");
-    one.onclick = function () {
+    one.addEventListener("click", function () {
         document.getElementById("resultsDisplay").innerText += "1";
-    };
+    }
+    );
     const two = document.getElementById("two");
-    two.onclick = function () {
+    two.addEventListener("click", function () {
         document.getElementById("resultsDisplay").innerText += "2";
-    };
+    }
+    );
     const three = document.getElementById("three");
-    three.onclick = function () {
+    three.addEventListener("click", function () {
         document.getElementById("resultsDisplay").innerText += "3";
-    };
+    }
+    );
     const four = document.getElementById("four");
-    four.onclick = function () {
+    four.addEventListener("click", function () {
         document.getElementById("resultsDisplay").innerText += "4";
-    };
+    }
+    );
     const five = document.getElementById("five");
-    five.onclick = function () {
+    five.addEventListener("click", function () {
         document.getElementById("resultsDisplay").innerText += "5";
-    };
+    }
+    );
     const six = document.getElementById("six");
-    six.onclick = function () {
+    six.addEventListener("click", function () {
         document.getElementById("resultsDisplay").innerText += "6";
-    };
+    }
+    );
     const multiplication = document.getElementById("multiplication");
-    multiplication.onclick = function () {
+    multiplication.addEventListener("click", function () {
         document.getElementById("resultsDisplay").innerText += "*";
-    };
+    }
+    );
     const minus = document.getElementById("minus");
-    minus.onclick = function () {
+    minus.addEventListener("click", function () {
         document.getElementById("resultsDisplay").innerText += "-";
-    };
+    }
+    );
     const plus = document.getElementById("plus");
-    plus.onclick = function () {
+    plus.addEventListener("click", function () {
         document.getElementById("resultsDisplay").innerText += "+";
-    };
+    }
+    );
     const division = document.getElementById("division");
-    division.onclick = function () {
+    division.addEventListener("click", function () {
         document.getElementById("resultsDisplay").innerText += "/";
-    };
+    }
+    );
     const decimal = document.getElementById("decimal");
-    decimal.onclick = function () {
+    decimal.addEventListener("click", function () {
         document.getElementById("resultsDisplay").innerText += ".";
-    };
+    }
+    );
     const equals = document.getElementById("equals");
-    equals.onclick = function () {
-        //document.getElementById("resultsDisplay").innerText += "=";
+    equals.addEventListener("click",function () {
         let operatorsList = document.getElementsByName("operator");
         let operator;
-        for (i=0; i<operatorsList.length; i++)
-        {
-            operator = operatorsList[i].value; 
-        
-        
-            switch (operator)  {
-                case "*":
-                    document.getElementById("resultsDisplay").innerText += " = " + (eval(document.getElementById("resultsDisplay").innerText));
-                    break;
-                case "/":
-                    document.getElementById("resultsDisplay").innerText += " = "+ (eval(document.getElementById("resultsDisplay").innerText));
-                    break;
-                case "+":
-                    document.getElementById("resultsDisplay").innerText += " = "+ (eval(document.getElementById("resultsDisplay").innerText));
-                    break;
-                case "-":
-                    document.getElementById("resultsDisplay").innerText += " = "+ (eval(document.getElementById("resultsDisplay").innerText));
-                    break;
-                default:
-                    document.getElementById("resultsDisplay").innerText = "Error";
-            }
-
+        for (i = 0; i < operatorsList.length; i++) {
+            operator = operatorsList[i].value;
         }
-        
-    };
+        let expressionCheck = /\d+(\*|\-|\+|\/)\d+/;
+        if (expressionCheck.test(document.getElementById("resultsDisplay").innerText)) {
+            document.getElementById("resultsDisplay").innerText += " = " + (eval(document.getElementById("resultsDisplay").innerText));
+            document.getElementById("resultsDisplay").style.color = "#016064";
+            document.getElementById("resultsDisplay").style.fontWeight = "900";
+        }
+        else   {
+           document.getElementById("resultsDisplay").innerText += " Format Error";
+           document.getElementById("resultsDisplay").style.color = "#FF0000";
+           document.getElementById("resultsDisplay").style.fontSize = "2.1em";         
+        }
+    }
+    );
     const clear = document.getElementById("clear");
     clear.onclick = function () {
         document.getElementById("resultsDisplay").innerText = " ";
